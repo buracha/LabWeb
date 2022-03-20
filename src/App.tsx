4257aa74d4ebbf38a53dfedaf307d0fd96daf2b7
buracha <jadsoncanuto6@gmail.com>
2022-03-20 18:23:56 -0300

    Navbar + Product Card + Catalog + react-router-dom install

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from 'components/Navbar';
+import './assets/styles/custom.scss';
+import './App.css';
+import Home from 'pages/Home';
+import Catalog from 'pages/Catalog';
+
+const App = () => {
+  return (
+    <Catalog />
+  );
+}
+
+export default App;
