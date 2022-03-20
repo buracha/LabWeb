189d84d5c15b4121aaf253803c4680c2929342a4
buracha <jadsoncanuto6@gmail.com>
2022-03-20 19:14:37 -0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import './assets/styles/custom.scss';
 import './App.css';
 import Home from 'pages/Home';
 import Catalog from 'pages/Catalog';
+import BRoutes from 'Routes';
 
 const App = () => {
   return (
-    <Catalog />
+    <BRoutes />
   );
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,10 @@
 import './styles.css';
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
-import Navbar from "components/Navbar";
 import ButtonIcon from 'components/ButtonIcon';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-      <>
-        <Navbar/>
         <div className="home-container">
             <div className="base-card home-card">
                 <div className="home-content-container">
@@ -14,7 +12,9 @@ const Home = () => {
                     <h1>Conheça o melhor catálago de produtos</h1>
                     <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
                     </div>
+                    <Link to= "/products">
                     <ButtonIcon />
+                    </Link>
                   </div>
                 <div className="home-image-container">
                     <MainImage />
@@ -22,7 +22,6 @@ const Home = () => {
             </div> 
 
         </div>
-      </>
     );
   }
   
